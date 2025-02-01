c78226256ac0376d9bd0e1d367df140d24e54d83
ambika84328 <grey.4e2@gmail.com>
2025-02-01 10:02:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer((req,res) => {
+    res.setHeader('Content-Type','text/html')
+
+    if(req.url=="/" && req.method=="GET"){
+        res.statusCode = 200;
+        res.end(
+        `
+            <form action="/" method="POST">
+                <label for="message">message:</label>
+                <input type="text" name="message"/>
+                <button type="submit">Submit</button>
+            </form>
+        `
+        )
+    }else if(req.url=="/" && req.method=="POST"){
+        let datachunks = [];
+
+        req.on('data',(chunks)=>{
+            console.log(chunks)
+            datachunks.push(chunks)
+        })
+
+        req.on('end',()=>{
+            let combinedBuffer = Buffer.concat(datachunks);
+            let formData = combinedBuffer.toString();
+            const formValues = formData.split("=")[1];
+            fs.writeFile('formData.txt',formValues,(err)=>{
+                if(err){
+                    console.log(err)
+                }else{
+                    res.statusCode = 302;
+                    res.setHeader('Location',"/");
+                    res.end();
+                }
+            })
+        })
+    }else{
+        res.statusCode = 404;
+        res.end("<h1>Page Not Found</h1>")
+    }
+})
+
+server.listen(3000,()=>{
+    console.log("listing in port 3000")
+})
+
