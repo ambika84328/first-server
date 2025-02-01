767836362d8349b32a53c92fd385a360ec45d2fc
ambika84328 <grey.4e2@gmail.com>
2025-02-01 16:24:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const http = require('http')
 const app = require('express')
+const bodyParser = require('body-parser')
 
-app.send((req,res,next)=>{
-    console.log("hello from first middleware");
-    next();
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.get('/', (req, res, next) =>{
+    res.send(
+        `
+        <form action="/product" method="POST">
+            <label for="title">Product</label>
+            <input type="text" name="title"/>
+            <br>
+            <label for="size">Size</label>
+            <input type="number" name="size"/>
+            <br>
+            <button type="submit">Add Product</button>
+        </form>
+        `
+    )
 })
 
-app.send((req,res,next)=>{
-    console.log("hello from second middleware");
+app.post("/",(req,res,next) => {
+    console.log(req.body)
+    res.redirect("/")
 })
 
 app.listen(3000)
